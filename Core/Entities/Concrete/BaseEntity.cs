d55469daa42cd56e760fb49289a2bbb2ae3ad4de
yasintorun <yasintorun4680@gmail.com>
2021-12-05 12:34:05 +0300

    Created Core Base Entity Classes
    Core katmanına Entity için base yapılar eklendi.

---
+++ Core/Entities/Concrete/BaseEntity.cs
@@ -0,0 +1,16 @@
+﻿using Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Entities.Concrete
+{
+    public class BaseEntity : IEntity
+    {
+        [Key]
+        public int Id { get; set; }
+    }
+}
