b1bdad9342d2612e78e6a6561d4abffafa8a3b9d
yasintorun <yasintorun4680@gmail.com>
2021-12-05 12:33:25 +0300

    Created Result Structures
    Result yapısı Oluşturuldu

---
+++ Core/Utils/Results/SuccessResult.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Utils.Results
+{
+    public class SuccessResult : Result
+    {
+        public SuccessResult() : base(true)
+        {
+
+        }
+        public SuccessResult(string message) : base(true, message)
+        {
+
+        }
+    }
+}
