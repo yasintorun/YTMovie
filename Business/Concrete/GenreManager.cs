7ba04d5141b2e4ac184922bfc69e286037a938dd
yasintorun <yasintorun4680@gmail.com>
2021-12-05 12:45:12 +0300

    Added Genre Entity
    Genre Entity Eklendi

---
+++ Business/Concrete/GenreManager.cs
@@ -0,0 +1,22 @@
+﻿using Business.Abstract;
+using Core.Business;
+using DataAccess.Abstract;
+using Entity.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.Concrete
+{
+    public class GenreManager : BaseManager<Genre>, IGenreService
+    {
+        private IGenreDal _genreDal;
+
+        public GenreManager(IGenreDal genreDal) : base(genreDal)
+        {
+            _genreDal = genreDal;
+        }
+    }
+}
