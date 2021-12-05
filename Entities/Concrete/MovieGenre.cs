c76d8205bca16ccf7e154548294691c862b22d63
yasintorun <yasintorun4680@gmail.com>
2021-12-05 12:45:28 +0300

    Added Genre and Movie Genre Entity

---
+++ Entities/Concrete/MovieGenre.cs
@@ -0,0 +1,16 @@
+﻿using Core.Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entity.Concrete
+{
+    public class MovieGenre : BaseEntity
+    {
+        public int MovieId { get; set; }
+
+        public int GenreId { get; set; }
+    }
+}
